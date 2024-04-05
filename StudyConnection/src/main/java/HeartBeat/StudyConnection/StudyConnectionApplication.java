bceeee58c415df4eae6501fbf26eaf663964b153
Kim Hani <32191261@dankook.ac.kr>
2024-04-05 16:16:31 +0900

    first commit

---
+++ StudyConnection/src/main/java/HeartBeat/StudyConnection/StudyConnectionApplication.java
@@ -0,0 +1,14 @@
+package HeartBeat.StudyConnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyConnectionApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(StudyConnectionApplication.class, args);
+	}
+
+}
---
+++ StudyConnection/src/test/java/HeartBeat/StudyConnection/StudyConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package HeartBeat.StudyConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
