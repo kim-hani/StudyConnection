7e75623e633e94f450ecd9269838a209720c2320
KimJongBin <jbb6514@naver.com>
2024-05-07 22:52:00 +0900

    Feat: logout

--- StudyConnection/src/main/java/HeartBeat/StudyConnection/controller/user/UserApiController.java
+++ StudyConnection/src/main/java/HeartBeat/StudyConnection/controller/user/UserApiController.java
@@ -14,9 +14,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -83,5 +88,12 @@ public class UserApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new AddUserResponse(savesUserId, savedUserName, resultMessage));
+    }
+
+    @PostMapping("/api/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
+        return ResponseEntity.ok()
+                .build();
     }
 }
