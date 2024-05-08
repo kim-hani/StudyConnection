f842659f41ce544d652d363d0007148b74e9e5b2
KimJongBin <jbb6514@naver.com>
2024-05-08 22:27:14 +0900

    Feat: dummy article for practice

---
+++ StudyConnection/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO studyArticle (author_name, content, limit_of_participants, available, title, created_at, updated_at)
+VALUES ('author1', 'content1', 5, true, 'title1', NOW(), NOW()),
+       ('author2', 'content2', 3, true, 'title2', NOW(), NOW()),
+       ('author3', 'content3', 7, true, 'title3', NOW(), NOW()),
+       ('author4', 'content4', 4, true, 'title4', NOW(), NOW()),
+       ('author5', 'content5', 6, true, 'title5', NOW(), NOW());
