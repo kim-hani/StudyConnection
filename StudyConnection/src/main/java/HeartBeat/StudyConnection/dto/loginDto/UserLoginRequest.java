4f02159396e559a358266bfbd21bad1c92bc11eb
KimJongBin <jbb6514@naver.com>
2024-05-28 21:39:09 +0900

    Feat: loginRequest dto solved

--- StudyConnection/src/main/java/HeartBeat/StudyConnection/dto/loginDto/UserLoginRequest.java
+++ StudyConnection/src/main/java/HeartBeat/StudyConnection/dto/loginDto/UserLoginRequest.java
@@ -2,8 +2,10 @@ package HeartBeat.StudyConnection.dto.loginDto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class UserLoginRequest {
     private String id;
