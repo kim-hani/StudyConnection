c9298cc9a05d1ff49ddc37be9525408e1e94efda
Kim Hani <32191261@dankook.ac.kr>
2024-06-04 17:00:38 +0900

    WebConfig 수정

--- StudyConnection/src/main/java/HeartBeat/StudyConnection/configuration/WebConfig.java
+++ StudyConnection/src/main/java/HeartBeat/StudyConnection/configuration/WebConfig.java
@@ -5,12 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080", "http://13.125.49.195:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowCredentials(true) // 쿠키 인증 요청 허용
+                .maxAge(3000);// 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+               // .allowedHeaders("*");
     }
 }
