bfcf18f4d3b98d54d4de4b009160a9ad58e7ac43
KimJongBin <jbb6514@naver.com>
2024-06-08 01:28:19 +0900

    Feat:WebSocket

--- StudyConnection/src/main/java/HeartBeat/StudyConnection/configuration/webSocket/WebSocketConfig.java
+++ StudyConnection/src/main/java/HeartBeat/StudyConnection/configuration/webSocket/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketHandler, "/api/chats/{studyId}").setAllowedOrigins("*");
+        registry.addHandler(webSocketHandler, "/api/chats/*").setAllowedOrigins("*").withSockJS();
     }
 }
 
