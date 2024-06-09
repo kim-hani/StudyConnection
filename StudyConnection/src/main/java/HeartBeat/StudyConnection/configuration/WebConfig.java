71c27ab390f9fd3f927049d8e0337424bef91eb1
KimJongBin <jbb6514@naver.com>
2024-06-10 02:12:36 +0900

    Feat: stduy recruitment

--- StudyConnection/src/main/java/HeartBeat/StudyConnection/configuration/WebConfig.java
+++ StudyConnection/src/main/java/HeartBeat/StudyConnection/configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://13.125.49.195:8080")
+                .allowedOrigins("http://localhost:8080", "http://13.125.49.195:8080", "http://localhost:8082")
                 .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH")
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000);// 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
