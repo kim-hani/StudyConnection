cf2880fec8351483350d5075bfa08190163cb107
KimJongBin <jbb6514@naver.com>
2024-06-10 02:15:41 +0900

    Feat: ChatController 수정

--- StudyConnection/src/main/java/HeartBeat/StudyConnection/controller/chattingController/ChattingController.java
+++ StudyConnection/src/main/java/HeartBeat/StudyConnection/controller/chattingController/ChattingController.java
@@ -5,6 +5,7 @@ import HeartBeat.StudyConnection.service.chatRoomService.ChattingRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -16,9 +17,8 @@ public class ChattingController {
 
     private final ChattingRoomService chattingRoomService;
 
-    @GetMapping("/api/chats")
-    public ResponseEntity<List<ChatRoom>> getUserChatRooms (Principal principal){
-        String userId = principal.getName();
+    @GetMapping("/api/chats/{userId}")
+    public ResponseEntity<List<ChatRoom>> getUserChatRooms (@PathVariable String userId){
         List<ChatRoom> chatRooms = chattingRoomService.findChatRoomsByUserId(userId);
 
         return ResponseEntity.ok()
